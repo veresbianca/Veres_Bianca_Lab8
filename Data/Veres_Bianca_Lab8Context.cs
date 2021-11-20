3d0c62ebec1e1ea8d10387519cb5c085e821be4d
Veres Bianca <biancaveres93@gmail.com>
2021-11-20 11:23:34 +0200

    Add project files.

---
+++ Data/Veres_Bianca_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Veres_Bianca_Lab8.Models;
+
+namespace Veres_Bianca_Lab8.Data
+{
+    public class Veres_Bianca_Lab8Context : DbContext
+    {
+        public Veres_Bianca_Lab8Context (DbContextOptions<Veres_Bianca_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Veres_Bianca_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Veres_Bianca_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Veres_Bianca_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Veres_Bianca_Lab8.Data;
+using Veres_Bianca_Lab8.Models;
+
+namespace Veres_Bianca_Lab8.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Veres_Bianca_Lab8.Data.Veres_Bianca_Lab8Context _context;
+
+        public DetailsModel(Veres_Bianca_Lab8.Data.Veres_Bianca_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
